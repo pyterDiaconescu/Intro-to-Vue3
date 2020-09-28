2e161d1fd336c0c9d3395cb814d5cd83f5fc529e
petrero <petrero@gmail.com>
2020-09-28 14:54:42 +0300

    11.2. Forms & v-model
    We want to bind the input fields to their respective data properties so that when the user fills out the form, we store their data locally.
    We’ll achieve this by adding the v-model directive to each of those input elements.
    Notice how on the <select> element, we used v-model.number, this is a modifier that typecasts the value as a number.

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -5,13 +5,13 @@ app.component('review-form', {
     <form class="review-form">
       <h3>Leave a review</h3>
       <label for="name">Name:</label>
-      <input type="text" id="name">
+      <input type="text" id="name" v-model="name">
       
       <label for="review">Review:</label>
-      <textarea id="review"></textarea>
+      <textarea id="review" v-model="review"></textarea>
       
       <label for="rating">Rating:</label>
-      <select id="rating">
+      <select id="rating" v-model.number="rating">
         <option>5</option>
         <option>4</option>
         <option>3</option>
@@ -20,5 +20,12 @@ app.component('review-form', {
       </select>
       <input type="submit" class="button" value="Submit">
     </form>
-  `
+  `,
+  data(){
+    return {
+      name: '',
+      review: '',
+      rating: null
+    }
+  }
 });
