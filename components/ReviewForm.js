b4b387a34d8587f96e30b6ec6142fe3448277774
petrero <petrero@gmail.com>
2020-09-28 14:52:31 +0300

    11.1. Forms & v-model
    Our Goal
    Create a form for users to add product reviews.
    
    🏳️ Introducing v-model
    At the beginning of this course, we learned about v-bind, which creates a one-way binding, from the data to the template. When working with forms, however, this one way binding isn’t enough. We also need to be binding from the template to the data.
    For example, when a user inputs their name into an input field, we want to record and store that value in our data. The v-model directive helps us achieve this, creating two-way data binding.
    To see this all in action, we’re going to create a new review-form component.
    
    🏳️ The review-form component
    We’ll add a new ReviewForm.js file into our components folder, and scaffold out the component.
    Inside of our template, notice these elements:
    
      <input id="name">
      <textarea id="review">
      <select id="rating">

---
+++ components/ReviewForm.js
@@ -0,0 +1,24 @@
+app.component('review-form', {
+  template:
+  /*html*/
+  `
+    <form class="review-form">
+      <h3>Leave a review</h3>
+      <label for="name">Name:</label>
+      <input type="text" id="name">
+      
+      <label for="review">Review:</label>
+      <textarea id="review"></textarea>
+      
+      <label for="rating">Rating:</label>
+      <select id="rating">
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+      </select>
+      <input type="submit" class="button" value="Submit">
+    </form>
+  `
+});
